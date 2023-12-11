0c6f11e3d6240f71c6eb2ad12caba4ff432874e2
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2023-12-11 12:03:52 +0100

    patch extractor

--- dating_preprocessing.py
+++ dating_preprocessing.py
@@ -1,32 +1,66 @@
 
-import glob
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+import matplotlib.patches as plt_patches
 from scipy.signal import find_peaks
+from enum import Enum
 
 
-class DatingPreprocessing:
+class PatchMethod(Enum):
+    RANDOM = 0
+    RANDOM_LINES = 1
+    SLIDING_WINDOW_LINES = 2
 
-    def __init__(self, path):
-        self.files = glob.glob(path, recursive=True)
-        self.num_lines_per_patch = 5
-        self.peak_distance = 50
-        self.plot = True
 
-    def show_img(self, img):
-        plt.imshow(img, cmap="gray")
-        plt.show()
+class PatchExtractor:
 
-    def read_img(self, path):
-        img = cv2.imread(path)
-        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def __init__(self, method=PatchMethod.RANDOM, num_lines_per_patch=5, 
+                 line_peak_distance=50, num_patches=20, plot=True, patch_size=256):
+        self.method = method
+        self.num_lines_per_patch = num_lines_per_patch
+        self.line_peak_distance = line_peak_distance
+        self.num_patches = num_patches
+        self.plot = plot
+        self.patch_size = patch_size
+        self.method_funcs = {PatchMethod.RANDOM: self._extract_random_patches,
+                             PatchMethod.RANDOM_LINES: self._extract_random_patches_based_on_lines,
+                             PatchMethod.SLIDING_WINDOW_LINES: self._extract_window_patches_based_on_lines}
     
-    def random_patch(self, img, size):
+    def extract_patches(self, img_path, method=None):
+        self._read_img(img_path)
+        if method:
+            self.method = method
+        self.patch_ls = []
+        func = self.method_funcs.get(self.method)
+        if not func:
+            print("Unknown method!")
+        else:
+            func()
+        return self.patch_ls
+    
+    def save_plot(self, title=None, show=False):
+        if not self.plot:
+            print("No plot! Plotting was not set during initialization!")
+            return
+        if not title:
+            title = str(self.method)
+        plt.savefig(title + ".png", dpi=300)
+        plt.savefig(title + ".pdf")
+        if show:
+            plt.show()
+
+    def _read_img(self, path):
+        img = cv2.imread(path)
+        self.img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if self.plot:
+            plt.imshow(self.img, cmap="gray")
+
+    def _get_random_patch(self, img, size):
         min_dim = min(img.shape)
         if min_dim < size:
-            print("Warning, image smaller than wanted size! Image size", img.shape, "but wanted", (size, size))
+            print("Warning, image smaller than wanted size! Image size",
+                  img.shape, "but wanted", (size, size))
             size = min_dim - 1
 
         max_width = img.shape[1] - size
@@ -36,67 +70,76 @@ class DatingPreprocessing:
         y = np.random.randint(0, max_height)
 
         return img[y:y+size, x:x+size], x, y
-    
-    def get_num_lines_in_img(self, img):
-        img_ones = 1 - (img / 255)
+
+    def _calc_num_lines_in_img(self):
+        img_ones = 1 - (self.img / 255)
 
         hist = img_ones.sum(axis=1)
         thresh = np.mean(hist)
-        peaks, _ = find_peaks(hist, distance=self.peak_distance)
-        peaks = peaks[hist[peaks] > thresh]
-        num_lines = len(peaks)
+        peaks, _ = find_peaks(hist, distance=self.line_peak_distance)
+        self.peaks = peaks[hist[peaks] > thresh]
+        self.num_lines = len(peaks)
 
         if self.plot:
-            plt.imshow(img, cmap="gray")
             for x in peaks:
                 plt.axhline(x, color="orangered", alpha=0.5)
-            plt.title(f"{num_lines} lines")
 
-        return num_lines, peaks
-    
-    def get_patch_size_based_on_lines(self, img, num_lines):
-        patch_size = int((img.shape[0] / num_lines) * self.num_lines_per_patch)
-        return min(patch_size, min(img.shape))
+    def _calc_patch_size_based_on_lines(self):
+        patch_size = int((self.img.shape[0] / self.num_lines) * self.num_lines_per_patch)
+        self.patch_size = min(patch_size, min(self.img.shape))
 
-    def get_random_patches_based_on_lines(self, img, num_patches=20):
-        patch_ls = []
-        num_lines, _ = self.get_num_lines_in_img(img)
-        patch_size = self.get_patch_size_based_on_lines(img, num_lines)
+    def _draw_rect(self, x, y):
+        rect = plt_patches.Rectangle((x, y), self.patch_size, self.patch_size, 
+                                     linewidth=2, edgecolor="blue", facecolor="none")
+        plt.gca().add_patch(rect)
 
-        for _ in range(num_patches):
-            patch, x, y = self.random_patch(img, patch_size)
-            patch_ls.append(patch)
-        
+    def _extract_random_patches(self):
+        for _ in range(self.num_patches):
+            patch, x, y = self._get_random_patch(self.img, self.patch_size)
+            self.patch_ls.append(patch)
+
             if self.plot:
-                rect = patches.Rectangle((x, y), patch_size, patch_size, linewidth=2, edgecolor="blue", facecolor="none")
-                plt.gca().add_patch(rect)
+                self._draw_rect(x, y)
 
-        return patch_ls
-    
-    def get_window_patches_based_on_lines(self, img):
-        num_lines, _ = self.get_num_lines_in_img(img)
-        patch_size = self.get_patch_size_based_on_lines(img, num_lines)
+        if self.plot:
+            plt.title(f"{self.num_patches} Random Patches at Size: {(self.patch_size, self.patch_size)}")
 
-        spots = int(img.shape[1] / patch_size)
-        y = 0
-        x = 0
+    def _extract_random_patches_based_on_lines(self):
+        self._calc_num_lines_in_img()
+        self._calc_patch_size_based_on_lines()
+        self._extract_random_patches()
+        if self.plot:
+            plt.title(f"{self.num_patches} Random Patches (Approx {self.num_lines_per_patch} Lines within each Patch)")
 
-        for i in range(int(img.shape[0] / patch_size)):
-            for j in range(spots):
-                patch = img[y:y+patch_size, x:x+patch_size]
+    def _extract_window_patches_based_on_lines(self):
+        self._calc_num_lines_in_img()
+        self._calc_patch_size_based_on_lines()
+
+        x_n = int(self.img.shape[1] / self.patch_size)
+        y_n = int(self.img.shape[0] / self.patch_size)
+
+        x_init = int((self.img.shape[1] % self.patch_size) / 2)
+        y_init = int((self.img.shape[0] % self.patch_size) / 2)
+        x = x_init
+        y = y_init
+
+        for _ in range(y_n):
+            for _ in range(x_n):
+                patch = self.img[y:y+self.patch_size, x:x+self.patch_size]
+                self.patch_ls.append(patch)
                 if self.plot:
-                    rect = patches.Rectangle((x, y), patch_size, patch_size, linewidth=2, edgecolor="blue", facecolor="none")
-                    plt.gca().add_patch(rect)
-            
-                x += patch_size
-            x = 0
-            y += patch_size
-        
-        #print(img.shape[1] / patch_size, img.shape[1] % patch_size)
+                    self._draw_rect(x, y)
+
+                x += self.patch_size
+            x = x_init
+            y += self.patch_size
+
+        if self.plot:
+            plt.title(f"Sliding Window Method (Approx {self.num_lines_per_patch} Lines within each Patch)")
 
 
-dp = DatingPreprocessing("t")
-img = dp.read_img("../MPS/Download/1375/MPS1375_0001.ppm")
-
-print(dp.get_window_patches_based_on_lines(img))
-plt.show()
+if __name__ == "__main__":
+    dp = PatchExtractor()
+    for i in range(10, 99):
+        patches = dp.extract_patches(f"../datasets/MPS/Download/1375/MPS1375_00{i}.ppm", PatchMethod.SLIDING_WINDOW_LINES)
+        dp.save_plot(show=True)
