ee2c0e909b6c3662555a59dd9664cb5dd9fbae31
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2024-02-14 16:39:37 +0100

    even more bottleneck

--- src/deep_dating/networks/autoencoder.py
+++ src/deep_dating/networks/autoencoder.py
@@ -6,8 +6,8 @@ import torch.nn as nn
 from torchvision import transforms
 from torchsummary import summary
 from pytorch_msssim import ssim
-from deep_dating.networks import ModelType
-from deep_dating.util import get_torch_device
+#from deep_dating.networks import ModelType
+#from deep_dating.util import get_torch_device
 
 
 class Autoencoder(nn.Module):
@@ -16,7 +16,7 @@ class Autoencoder(nn.Module):
         super(Autoencoder, self).__init__()
         self.learning_rate = learning_rate
         self.model_name = "autoencoder"
-        self.model_type = ModelType.AUTOENCODER
+        #self.model_type = ModelType.AUTOENCODER
         self.input_size = input_size
 
         self.encoder = nn.Sequential(
@@ -38,22 +38,22 @@ class Autoencoder(nn.Module):
             nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            # nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
         )
         
         self.decoder = nn.Sequential(
             # nn.Linear(512, 65536),
             # nn.ReLU(),
             # nn.Unflatten(1, (256, 16, 16)),
-            # nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
-            # nn.BatchNorm2d(512),
-            # nn.ReLU(),
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.BatchNorm2d(256),
+            nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.ConvTranspose2d(256, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
@@ -119,8 +119,8 @@ class Autoencoder(nn.Module):
         print("Model loading completed!")
 
     def summary(self):
-        # summary(self.encoder, (1, 512, 512))
-        # summary(self.decoder, (512, 8, 8)) #(256, 32, 32)
+        summary(self.encoder, (1, 512, 512))
+        summary(self.decoder, (512, 4, 4)) #(256, 32, 32)
         pass
     
 
