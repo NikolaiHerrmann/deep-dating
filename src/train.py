6875f5722243dcfd0f590688c3cd2da3c38a3cf6
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2024-03-06 08:05:52 +0100

    running next split

--- src/train.py
+++ src/train.py
@@ -22,12 +22,12 @@ def train_dating_cnn():
     dataset = DatasetName.MPS
 
     cross_val = CrossVal(dataset, preprocess_ext="_Set_P1_Bin_299")
-    trainer = DatingTrainer("Inception for P2 for MPS with bin images", num_epochs=50, patience=3)
+    trainer = DatingTrainer("Inception for P2 for MPS with bin images", num_epochs=50, patience=3, exp_name="Mar5-21-59-18")
     n_splits = 5
     batch_size = 32
 
     # leave empty, just in case program crashes and need to re-run
-    avoid_splits = [] 
+    avoid_splits = [0, 3, 4] 
 
     for i, (X_train, y_train, X_val, y_val) in enumerate(cross_val.get_split(n_splits=n_splits)):
         
@@ -45,7 +45,6 @@ def train_dating_cnn():
         trainer.train(model, train_loader, val_loader, i)
 
         run_dating_cnn_predictions(model, trainer.best_model_path, train_loader, val_loader, i)
-        break
 
 
 def train_autoencoder():
@@ -73,6 +72,6 @@ def train_classifier():
 
 
 if __name__ == "__main__":
-    train_dating_cnn()
+    #train_dating_cnn()
     #train_autoencoder()
-    #train_classifier()
+    train_classifier()
